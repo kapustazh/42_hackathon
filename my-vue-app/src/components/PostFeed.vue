0464326f825a768771647bce48b39912e57d3224
Marcel Mantz <mmantz@3-C-3.42heilbronn.de>
2025-10-20 22:25:53 +0200

    big change

---
+++ my-vue-app/src/components/PostFeed.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+import PostItem from './PostItem.vue';
+
+defineProps({
+  posts: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="post-list">
+    <PostItem v-for="post in posts" :key="post.id" :post="post" />
+    <p v-if="posts.length === 0">No main posts to display.</p>
+  </div>
+</template>
