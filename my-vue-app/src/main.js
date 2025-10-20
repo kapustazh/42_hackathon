ea5a1b66399ce661ca660e5122b93dfa9df451c2
Marcel Mantz <mmantz@3-C-3.42heilbronn.de>
2025-10-20 22:34:02 +0200

    lib in main.js

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { createApp } from 'vue';
+import App from './App.vue';
+// 1. Import Pinia function
+import { createPinia } from 'pinia';
+
+// 2. Create the Vue application instance
+const app = createApp(App);
+
+// 3. Create the Pinia store instance
+const pinia = createPinia();
+
+// 4. Tell Vue to use Pinia (registers the store globally)
+app.use(pinia);
+
+// 5. Mount the application
+app.mount('#app');
+
 createApp(App).mount('#app')
